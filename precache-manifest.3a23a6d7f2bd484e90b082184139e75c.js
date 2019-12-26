cd6750512dc3e7585d27dc16a04d4525cc810191
Adam Jeliński <adas.jelinski@gmail.com>
2019-12-26 14:11:04 +0100

    Updates

---
+++ precache-manifest.3a23a6d7f2bd484e90b082184139e75c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c157254502895f9c456cfb65d5776a",
+    "url": "/newspaper-graphs/index.html"
+  },
+  {
+    "revision": "c31500b46c796a90867b",
+    "url": "/newspaper-graphs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9d7813f05c9212e61015",
+    "url": "/newspaper-graphs/static/js/2.9d913222.chunk.js"
+  },
+  {
+    "revision": "6dbd9e7d4b65b27382d6cb0964c77a31",
+    "url": "/newspaper-graphs/static/js/2.9d913222.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c31500b46c796a90867b",
+    "url": "/newspaper-graphs/static/js/main.d50d4d4d.chunk.js"
+  },
+  {
+    "revision": "f3e0e7ab7585f78ae800",
+    "url": "/newspaper-graphs/static/js/runtime-main.ffef5b93.js"
+  }
+]);
